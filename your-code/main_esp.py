ef00844b4f2ac71dba80af4bb10ec2341e9b9469
ivanseldas <ivanseldasp@gmail.com>
2024-04-25 23:43:28 +0200

    all finished

--- your-code/main_esp.py
+++ your-code/main_esp.py
@@ -1,72 +1,56 @@
 #1. Importa el paquete NUMPY bajo el nombre np.
-
-#[tu código aquí]
+import numpy as np
 
 
 #2. Imprime la versión de NUMPY y la configuración.
-
-#[tu código aquí]
-
+print(np.__version__)
 
 #3. Genera un array tridimensional de 2x3x5 con valores aleatorios. Asigna el array a la variable "a"
 # Desafío: hay al menos tres maneras fáciles que usan numpy para generar arrays aleatorios. ¿Cuántas formas puedes encontrar?
-
-#[tu código aquí]
+a = np.random.random((2, 3, 5))
 
 #4. Imprime a.
+print(a)
 
-#[tu código aquí]
 #5. Crea un array tridimensional de 5x2x3 con todos los valores igual a 1.
-#Asigna el array a la variable "b"
-
-#[tu código aquí]
+b = np.ones((5, 2, 3))
 
 #6. Imprime b.
-
-#[tu código aquí]
+print(b)
 
 #7. ¿Tienen a y b el mismo tamaño? ¿Cómo lo demuestras en código Python?
-
-#[tu código aquí]
+print(a.size == b.size)
 
 #8. ¿Es posible sumar a y b? ¿Por qué sí o por qué no?
-
-#[tu código aquí]
+# No porque las matrices son de diferente forma
 
 
 #9. Transpone b para que tenga la misma estructura que a (es decir, se convierta en un array de 2x3x5). Asigna el array transpuesto a la variable "c".
 
-#[tu código aquí]
+c = np.transpose(b, axes=(1, 2, 0))
+print(c.shape)
 
 #10. Intenta sumar a y c. Ahora debería funcionar. Asigna la suma a la variable "d". Pero, ¿por qué funciona ahora?
-
-#[tu código aquí]
+d = a + c
 
 #11. Imprime a y d. ¿Notas la diferencia y la relación entre los dos arrays en términos de los valores? Explica.
-
-#[tu código aquí]
-
+print(a)
+print(b)
+print(a.size == d.size)
 
 #12. Multiplica a y c. Asigna el resultado a e.
-
-#[tu código aquí]
-
+e = np.multiply(a, c)
 
 #13. ¿Es e igual a a? ¿Por qué sí o por qué no?
-
-#[tu código aquí]
-
-
+print(a == e)
 
 #14. Identifica los valores máximos, mínimos y medios en d. Asigna esos valores a las variables "d_max", "d_min" y "d_mean"
-
-#[tu código aquí]
-
+d_max = np.max(d)
+d_min = np.min(d)
+d_mean = np.mean(d)
 
 #15. Ahora queremos etiquetar los valores en d. Primero crea un array vacío "f" con la misma forma (es decir, 2x3x5) que d usando `np.empty`.
-
-#[tu código aquí]
-
+f = np.empty((2, 3, 5))
 
 """
 #16. Rellena los valores en f. Para cada valor en d, si es mayor que d_min pero menor que d_mean, asigna 25 al valor correspondiente en f.
@@ -78,10 +62,11 @@ Al final, f debería tener solo los siguientes valores: 0, 25, 50, 75 y 100.
 Nota: no necesitas usar Numpy en esta pregunta.
 """
 
-#[tu código aquí]
-
-
-
+f = np.where((d > d_min) & (d < d_mean), 25, f)
+f = np.where((d > d_mean) & (d < d_max), 75, f)
+f = np.where((d == d_mean), 50, f)
+f = np.where((d == d_min), 0, f)
+f = np.where((d == d_max), 100, f)
 
 """
 #17. Imprime d y f. ¿Tienes el f esperado?
@@ -103,10 +88,8 @@ array([[[ 75.,  75.,  75.,  25.,  75.],
         [ 75.,  75.,  75.,  75.,  75.],
         [ 25.,  75.,   0.,  75.,  75.]]])
 """
-
-#[tu código aquí]
-
-
+print(d)
+print(f)
 
 """
 #18. Pregunta de bonificación: en lugar de usar números (es decir, 0, 25, 50, 75 y 100), ¿cómo usar valores de cadena 
@@ -120,5 +103,10 @@ array([[[ 'D',  'D',  'D',  'B',  'D'],
         [ 'B',  'D',   'A',  'D', 'D']]])
 De nuevo, no necesitas Numpy en esta pregunta.
 """
-
-#[tu código aquí]
+g = np.empty(f.shape)
+g = np.where((d > d_min) & (d < d_mean), 'B', g)
+g = np.where((d > d_mean) & (d < d_max), 'D', g)
+g = np.where((d == d_mean), 'C', g)
+g = np.where((d == d_min), 'A', g)
+g = np.where((d == d_max), 'E', g)
+print(g)
